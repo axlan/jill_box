b7da3ee5d8ba5e14bc26f46b6c77ed5af8ecf3af
jdiamond <feros32@gmail.com>
2020-03-23 07:44:33 +0000

    Added basic game logic

---
+++ jill_box/game.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+# NOTE: NEEDS SYNCHRONIZATION FOR MULTITHREADING
+
+import random
+import string
+from enum import Enum, auto
+from abc import ABC,abstractmethod 
+
+from typing import (
+    Dict,
+    List,
+    Optional,
+    Tuple
+)
+
+def _random_id() -> str:
+    ascii = string.ascii_letters
+    return "".join(random.choices(ascii, k=GameGateway.NUM_ROOM_LETTERS))
+
+class JoinReturnCodes(Enum):
+    SUCCESS = auto()
+    NAME_IN_USE = auto()
+    ROOM_NOT_FOUND = auto()
+
+class GetReturnCodes(Enum):
+    SUCCESS = auto()
+    ROOM_NOT_FOUND = auto()
+    NAME_NOT_FOUND = auto()
+
+class StartReturnCodes(Enum):
+    SUCCESS = auto()
+    TOO_FEW_PLAYERS = auto()
+    ALREADY_STARTED = auto()
+    ROOM_NOT_FOUND = auto()
+
+class InteractReturnCodes(Enum):
+    SUCCESS = auto()
+    INVALID_DATA = auto()
+    WRONG_STATE = auto()
+    ROOM_NOT_FOUND = auto()
+    PLAYER_NOT_FOUND = auto()
+
+
+class Player:
+    def __init__(self):
+        pass
+
+class Room(ABC):
+    def __init__(self):
+        self.players: Dict[str, Player] = {}
+    def add_player(self, name) -> bool:
+        if name in self.players.keys():
+            return False
+        self.players[name] = Player()
+        return True
+    @abstractmethod
+    def start(self) -> StartReturnCodes:
+        pass
+    @abstractmethod
+    def get_room_state(self, player) -> Tuple[InteractReturnCodes, str]:
+        pass
+    @abstractmethod
+    def submit_data(self, player, data) -> InteractReturnCodes:
+        pass
+
+class GameGateway:
+
+    NUM_ROOM_LETTERS = 4
+
+    def __init__(self):
+        self.rooms: Dict[str, Room] = {}
+
+    def room_start(self, room) -> StartReturnCodes:
+        if room not in self.rooms:
+            return StartReturnCodes.ROOM_NOT_FOUND
+        return self.rooms[room].start()
+
+    def new_game(self, room_class) -> str:
+        room = _random_id()
+        self.rooms[room] = room_class()
+        return room
+
+    def join_room(self, room, name) -> JoinReturnCodes:
+        try:
+            success = self.rooms[room].add_player(name)
+            if success:
+                return JoinReturnCodes.SUCCESS
+            else:
+                return JoinReturnCodes.NAME_IN_USE
+        except:
+            return JoinReturnCodes.ROOM_NOT_FOUND
+
+    def get_room_state(self, room, name=None) -> Tuple[InteractReturnCodes, str]:
+        if room in self.rooms:
+            if name is None or name in self.rooms[room].players:
+                return self.rooms[room].get_room_state(name)
+            else:
+                return (InteractReturnCodes.PLAYER_NOT_FOUND, '')
+        return (InteractReturnCodes.ROOM_NOT_FOUND, '')
+
+    def submit_data(self, room, name, data) -> InteractReturnCodes:
+        if room in self.rooms:
+            if name in self.rooms[room].players:
+                return self.rooms[room].submit_data(name, data)
+            else:
+                return InteractReturnCodes.PLAYER_NOT_FOUND
+        return InteractReturnCodes.ROOM_NOT_FOUND
---
+++ jill_box/game_test.py
@@ -0,0 +1,175 @@
+
+import string
+import random
+from enum import Enum, auto
+from collections import defaultdict
+
+from typing import (
+    List,
+    Dict,
+    Tuple
+)
+
+from jill_box.game import GameGateway, Room, StartReturnCodes, InteractReturnCodes
+
+
+def _load_prompts() -> List[Tuple[str, str]]:
+    prompts :List[Tuple[str, str]] = []
+    with open('jill_box/data/questions.txt') as fd:
+        for line in fd.readlines():
+            prompts.append(tuple(line.split('\t')))
+    return prompts
+
+
+class TestRoom(Room):
+
+    class State(Enum):
+        WAITING_TO_START = auto()
+        COLLECTING_ANSWERS = auto()
+        VOTING = auto()
+        SHOWING_RESULTS = auto()
+
+    ROUNDS = 3 
+
+    PROMPTS = _load_prompts()
+  
+    def __init__(self):
+        super().__init__()
+        
+        self.prompts = random.sample(TestRoom.PROMPTS, TestRoom.ROUNDS)
+        self.state = TestRoom.State.WAITING_TO_START
+        self.round = 1
+        self.answers: Dict[str, str] = {}
+        self.votes: Dict[str, str] = {}
+        self.vote_orders: Dict[str, List[str]] = {}
+        self.scores: Dict[str, int] = defaultdict(int)
+        self.confirmed: Dict[str, bool] = {}
+
+    def start(self) -> StartReturnCodes:
+        if self.state != TestRoom.State.WAITING_TO_START:
+            return StartReturnCodes.ALREADY_STARTED
+        if len(self.players) < 3:
+            return StartReturnCodes.TOO_FEW_PLAYERS
+        self.state = TestRoom.State.COLLECTING_ANSWERS
+        return StartReturnCodes.SUCCESS
+
+
+    def get_prompt(self) -> str:
+        return self.prompts[self.round][0]
+
+    def get_anwers(self, user) -> List[str]:
+        if user is None:
+            return random.sample(list(self.answers.values()), len(self.answers))
+        else:
+            return [ self.answers[p] for p in self.vote_orders[user] ]
+    
+    def __start_voting(self):
+        self.state = TestRoom.State.VOTING
+        for player in self.players:
+            other_players = [ k for k in self.answers.keys() if k != player ]
+            self.vote_orders[player] = random.sample(other_players, len(other_players))
+
+    def __show_results(self):
+        self.state = TestRoom.State.SHOWING_RESULTS
+        new_scores = self.__votes_to_score()
+        for name, score in new_scores.items():
+            self.scores[name] += score
+
+    def __votes_to_score(self):
+        scores = { u:0 for u in self.players }
+        for vote in self.votes.values():
+            scores[vote] += 1 
+        return scores
+
+    def __next_round(self):
+        self.round += 1
+        if self.round < TestRoom.ROUNDS:
+            self.state = TestRoom.State.COLLECTING_ANSWERS
+            self.answers: Dict[str, str] = {}
+            self.votes: Dict[str, str] = {}
+            self.vote_orders: Dict[str, List[str]] = {}
+            self.confirmed: Dict[str, bool] = {}
+        else:
+            pass
+
+
+    def submit_data(self, player, data) -> InteractReturnCodes:
+        try:
+            if self.state == TestRoom.State.COLLECTING_ANSWERS:
+                self.answers[player] = data['answer']
+                if len(self.answers) == len(self.players):
+                    self.__start_voting()
+            elif self.state == TestRoom.State.VOTING:
+                vote = int(data['vote'])
+                self.votes[player] = self.vote_orders[player][vote]
+                if len(self.votes) == len(self.players):
+                    self.__show_results()
+            elif self.state == TestRoom.State.SHOWING_RESULTS:
+                self.confirmed[player] = True
+                if len(self.confirmed) == len(self.players):
+                    self.__next_round()
+            else:
+                return InteractReturnCodes.WRONG_STATE
+            return InteractReturnCodes.SUCCESS
+        except:
+            return InteractReturnCodes.INVALID_DATA
+
+    def get_room_state(self, player) -> Tuple[InteractReturnCodes, str]:
+        if self.state == TestRoom.State.COLLECTING_ANSWERS:
+            return (InteractReturnCodes.SUCCESS, self.get_prompt())
+        elif self.state == TestRoom.State.VOTING:
+            answers = str(self.get_anwers(player))
+            return (InteractReturnCodes.SUCCESS, answers)
+        elif self.state == TestRoom.State.SHOWING_RESULTS:
+            ret = str({'earned': self.__votes_to_score(), 'total': self.scores})
+            return (InteractReturnCodes.SUCCESS, ret)
+        return (InteractReturnCodes.WRONG_STATE, '')
+
+gateway = GameGateway()
+
+room = gateway.new_game(TestRoom)
+
+name1 = "tester1"
+name2 = "tester2"
+name3 = "tester3"
+
+print(gateway.join_room(room, name1))
+print(gateway.join_room(room, name2))
+print(gateway.join_room(room, name3))
+
+print(gateway.room_start(room))
+
+print(gateway.get_room_state(room))
+
+print(gateway.submit_data(room, name1, {'answer': 'A'}))
+print(gateway.submit_data(room, name2, {'answer': 'B'}))
+print(gateway.submit_data(room, name3, {'answer': 'C'}))
+
+print(gateway.get_room_state(room))
+print(gateway.get_room_state(room, name2))
+
+print(gateway.submit_data(room, name1, {'vote': '1'}))
+print(gateway.submit_data(room, name2, {'vote': '0'}))
+print(gateway.submit_data(room, name3, {'vote': '1'}))
+
+print(gateway.get_room_state(room))
+
+print(gateway.submit_data(room, name1, {}))
+print(gateway.submit_data(room, name2, {}))
+print(gateway.submit_data(room, name3, {}))
+
+
+print(gateway.get_room_state(room))
+
+print(gateway.submit_data(room, name1, {'answer': 'A2'}))
+print(gateway.submit_data(room, name2, {'answer': 'B2'}))
+print(gateway.submit_data(room, name3, {'answer': 'C2'}))
+
+print(gateway.get_room_state(room))
+print(gateway.get_room_state(room, name2))
+
+print(gateway.submit_data(room, name1, {'vote': '0'}))
+print(gateway.submit_data(room, name2, {'vote': '0'}))
+print(gateway.submit_data(room, name3, {'vote': '1'}))
+
+print(gateway.get_room_state(room))
